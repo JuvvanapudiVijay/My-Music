8ef4d9f7b4ee74296c1ad6146effb75856855fa7
vijay juvvanapudi <juvvanapudivijay123@gmail.com>
2024-07-24 11:57:13 +0530

    files are added

---
+++ music-list.js
@@ -0,0 +1,62 @@
+let allMusic = [
+    {
+        name: "Kurchini Madathapetti",
+        artist: "SS Thaman",
+        img: "music-11",
+        src: "music-11"
+    },
+    {
+        name: "Hukum Tiger Ka Hukum",
+        artist: "Anirudh Ravi Chanran",
+        img: "music-12",
+        src: "music-12"
+    },
+    {
+        name: "Prathi Gaadhalo",
+        artist: "Salaar",
+        img: "music-13",
+        src: "music-13"
+    },
+    {
+        name: "Poolamme Pilla",
+        artist: "Hanuman",
+        img: "music-14",
+        src: "music-14"
+    },
+    {
+        name: "Ammayi",
+        artist: "Animal",
+        img: "music-15",
+        src: "music-15"
+    },
+    {
+        name: "Teliyade Teliyade",
+        artist: "Teliyade",
+        img: "music-16",
+        src: "music-16"
+    },
+    {
+        name: "Gulab Sadi",
+        artist: "Gulab Sadi",
+        img: "music-17",
+        src: "music-17"
+    },
+    {
+        name: "Jada",
+        artist: "Chor Bajar",
+        img: "music-18.jpeg",
+        src: "music-18"
+    },
+    {
+        name: "Sugar and Brownies",
+        artist: "Dharia",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "Faded",
+        artist: "Alan Walker",
+        img: "music-10",
+        src: "music-10"
+    },
+];
